96ef9517a2d8b4dc2e3887bed18dba3e810cd2a0
LXH-97 <18946081317@163.com>
2018-03-27 22:09:37 +0800

    commit

---
+++ Web_Scraping_with_Python/Chapter2/articleSpider.py
@@ -0,0 +1,20 @@
+from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy import Spider
+from wikiSpider import Spider, Article
+
+class ArticleSpider(Spider):
+    name = "article"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["http://en.wikipedia.org/wiki/Main_Page",
+                  "http://en.wikipedia.org/wiki/Python_%28programming_languge%29"]
+    rules = [Rule(SgmlLinkExtractor(allow=('(/wiki/)((?!:).)*$'),),
+                                        callback="parse_item", follow=True)]
+
+
+    def parse(self, response):
+        item = Article()
+        title = response.xpath('//h1/text()')[0].extract()
+        print("Tittle is: " + title)
+        item['title'] = title
+        return item
